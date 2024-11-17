0cab62574a93b5a2fc5708802d3a413579facacd
Sohom Ghosh <139136976+sohom-21@users.noreply.github.com>
2024-11-17 13:03:27 +0530

    Create Tabs.jsx

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+export function Tabs(props) {
+         const { todos } = props;
+  const tabs = ["All", "Open", "Completed"];
+  return (
+    <nav className="tab-container">
+      {tabs.map((tab, tabIndex) => {
+        const numberOfTodos = () => {
+          if(tab === "All"){
+            return ( todos.length)
+          }
+          else if(tab === 'Open'){
+            return ( todos.filter((todo) => todo.completed === false).length )
+          }
+          else if(tab === 'Completed'){
+            return ( todos.filter((todo) => todo.completed === true).length )
+          }
+        } 
+        return (
+          <button key={tabIndex} className="tab-button">
+            <h4>{tab}<span>({numberOfTodos()})</span></h4>
+          </button>
+        );
+      })}
+    </nav>
+  );
+}
+
+Tabs.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      completed: PropTypes.bool.isRequired,
+      // include other required todo properties here
+    })
+  ).isRequired
+};
