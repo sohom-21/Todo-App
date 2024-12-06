20dffa5a748b4f79d168e60ab134dc1f29c6c344
Sohom Ghosh <139136976+sohom-21@users.noreply.github.com>
2024-12-06 14:27:00 +0530

    some changes but not completed

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,5 +1,17 @@
-export function TodoCard() {
+export function TodoCard(props) {
+         const { todoIndex, todos} = props
+         const todo = todos[todoIndex]
+         console.log(todo)
          return (
-                  <div></div>
+                  <div className="card todo-item">
+                           <p>{todo.input}</p>
+                           <div className="todo-buttons"></div>
+                           <button disabled={todo.completed}>
+                                    <h6>Done</h6>
+                           </button>
+                           <button>
+                                    <h6>Delete</h6>
+                           </button>
+                  </div>
          )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,27 @@
 import { TodoCard } from "./TodoCard";
 
-export function TodoList() {
-         return (
-                  <div>
-                           <TodoCard />
-                  </div>
-         )
+export function TodoList(props) {
+  const { todos } = props;
+  const tabs = "All"
+  const filteredTodosList= () =>{
+         if(tabs === "All"){
+            return todos;
+         }
+         else if(tabs === 'Completed'){
+            return todos.filter((todo) => todo.completed === true)
+         }
+         else if(tabs === 'Open'){
+               return todos.filter((todo) => todo.completed === false)    
+         }
+  }
+  return (
+    <>
+      {filteredTodosList.map((todo, todoIndex) => {
+        return <TodoCard 
+        key={todoIndex} 
+        todoIndex={todoIndex} 
+        todo={todo} />;
+      })}
+    </>
+  );
 }
